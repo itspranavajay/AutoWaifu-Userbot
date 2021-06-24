cfb65e8846e5b252465a774c79d9888f59c399e5
RedAura <86167689+MoeZilla@users.noreply.github.com>
2021-06-25 01:18:05 +0530

    Create __init__.py

---
+++ WaifuUserbot/__init__.py
@@ -0,0 +1,9 @@
+import os
+import time
+from pyrogram import Client, errors
+
+API_ID = os.environ.get("API_ID", None)
+API_HASH = os.environ.get("API_HASH", None)
+SESSION = os.environ.get("SESSION", None)
+
+Waifu = Client(SESSION, api_id=API_ID, api_hash=API_HASH)
