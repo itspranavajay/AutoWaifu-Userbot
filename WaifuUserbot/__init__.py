934f15179f0ae02ef875fc40b6aab2cd77d373f2
RedAura <86167689+MoeZilla@users.noreply.github.com>
2021-06-25 01:43:44 +0530

    Update __init__.py

--- WaifuUserbot/__init__.py
+++ WaifuUserbot/__init__.py
@@ -5,5 +5,6 @@ from pyrogram import Client, errors
 API_ID = os.environ.get("API_ID", None)
 API_HASH = os.environ.get("API_HASH", None)
 SESSION = os.environ.get("SESSION", None)
+RELAY = os.environ.get("RELAY", None)
 
 Waifu = Client(SESSION, api_id=API_ID, api_hash=API_HASH)
